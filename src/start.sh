cfab987bb68c9fcd4795503e320dd8e3b6fe4995
Luandro <luandro@digital-democracy.org>
2023-05-09 17:57:51 -0300

    Inject into /etc/hosts instead of copying file

--- src/start.sh
+++ src/start.sh
@@ -3,5 +3,21 @@
 # Import env variables
 . .env_vars
 
+file=firstboot
+
+if [ ! -e "$file" ] ; then
+    echo "First run!"
+    echo "================================"
+    echo "Adding to /etc/hosts"
+    sed -i "192.168.42.1  kit.com edt.com edt.net edt.local kitdt.com kitdt.net kit kitdt edt" /etc/hosts
+    touch "$file"
+fi
+
+if [ ! -w "$file" ] ; then
+    echo "Hosts already added"
+    exit 1
+fi
+
+
 # Execute Python script
 exec python3 -u run.py
